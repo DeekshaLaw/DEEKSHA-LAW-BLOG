e8a5e350e24f53582a07b84d795e47b0d87a406f
RSM Harsha <postboxofharsha@gmail.com>
2025-12-29 14:47:20 +0530

    changed email service from nodemailer to brevo

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,34 +1,26 @@
-const nodemailer = require('nodemailer');
+const axios = require("axios");
 
-/**
- * Send email using nodemailer
- * @param {Object} options - Email options
- * @param {String} options.to - Recipient email
- * @param {String} options.subject - Email subject
- * @param {String} options.text - Plain text content
- * @param {String} options.html - HTML content
- */
-const sendEmail = async (options) => {
-  // Create a transporter
-  const transporter = nodemailer.createTransport({
-    service: process.env.EMAIL_SERVICE,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD
+const sendEmail = async ({ to, subject, text, html }) => {
+  await axios.post(
+    "https://api.brevo.com/v3/smtp/email",
+    {
+      sender: {
+        email: process.env.EMAIL_FROM,
+        name: "Deeksha Law"
+      },
+      to: [{ email: to }],
+      subject,
+      textContent: text,
+      htmlContent: html,
+    },
+    {
+      headers: {
+        "api-key": process.env.BREVO_API_KEY,
+        "Content-Type": "application/json",
+      },
+      timeout: 5000,
     }
-  });
-
-  // Define the email options
-  const mailOptions = {
-    from: process.env.EMAIL_FROM,
-    to: options.to,
-    subject: options.subject,
-    text: options.text,
-    html: options.html || null
-  };
-
-  // Send the email
-  await transporter.sendMail(mailOptions);
+  );
 };
 
-module.exports = sendEmail; 
+module.exports = sendEmail;
